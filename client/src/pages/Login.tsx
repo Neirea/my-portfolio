7ce2205bef1925207fa754ac06f312c1aaf7b3f7
Neirea <neireasoultix@gmail.com>
2022-11-07 16:59:21 +0200

    align center login button icon && text

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -7,76 +7,82 @@ import type { LocationState } from "../types/appTypes";
 import { useCurrentLocation } from "../utils/useCurrentLocation";
 
 const Login = () => {
-	const location = useLocation<LocationState>();
-	const queries = useCurrentLocation();
-	const errorQuery = queries.get("error");
-	const fromLocation = location.state?.from;
-	const fromUrl =
-		fromLocation && fromLocation.pathname.length > 1
-			? fromLocation.pathname.slice(1) + fromLocation.search
-			: "";
+    const location = useLocation<LocationState>();
+    const queries = useCurrentLocation();
+    const errorQuery = queries.get("error");
+    const fromLocation = location.state?.from;
+    const fromUrl =
+        fromLocation && fromLocation.pathname.length > 1
+            ? fromLocation.pathname.slice(1) + fromLocation.search
+            : "";
 
-	const handleLoginGithub = async () => {
-		window.open(
-			`${process.env.REACT_APP_SERVER_URL}/api/auth/login/github?path=${fromUrl}`,
-			"_self"
-		);
-	};
+    const handleLoginGithub = async () => {
+        window.open(
+            `${process.env.REACT_APP_SERVER_URL}/api/auth/login/github?path=${fromUrl}`,
+            "_self"
+        );
+    };
 
-	const handleLoginGoogle = async () => {
-		window.open(
-			`${process.env.REACT_APP_SERVER_URL}/api/auth/login/google?path=${fromUrl}`,
-			"_self"
-		);
-	};
+    const handleLoginGoogle = async () => {
+        window.open(
+            `${process.env.REACT_APP_SERVER_URL}/api/auth/login/google?path=${fromUrl}`,
+            "_self"
+        );
+    };
 
-	//check if user is logged in then redirect to main page
-	return (
-		<LoginWrapper>
-			<div className="login-container">
-				<h4>Sign in with</h4>
-				{errorQuery === "login_failed" && <AlertMsg>Login failed</AlertMsg>}
-				<LoginButton className="btn-github" onClick={handleLoginGithub}>
-					<FaGithub />
-					<span>{"Github"}</span>
-				</LoginButton>
-				<LoginButton className="btn-google" onClick={handleLoginGoogle}>
-					<FaGoogle />
-					<span>{"Google"}</span>
-				</LoginButton>
-			</div>
-		</LoginWrapper>
-	);
+    //check if user is logged in then redirect to main page
+    return (
+        <LoginWrapper>
+            <div className="login-container">
+                <h4>Sign in with</h4>
+                {errorQuery === "login_failed" && (
+                    <AlertMsg>Login failed</AlertMsg>
+                )}
+                <LoginButton className="btn-github" onClick={handleLoginGithub}>
+                    <FaGithub />
+                    <span>{"Github"}</span>
+                </LoginButton>
+                <LoginButton className="btn-google" onClick={handleLoginGoogle}>
+                    <FaGoogle />
+                    <span>{"Google"}</span>
+                </LoginButton>
+            </div>
+        </LoginWrapper>
+    );
 };
 
 const LoginWrapper = styled.main`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: 80vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80vh;
 
-	.login-container {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		position: relative;
-		padding: 2rem 2.5rem;
-		padding-top: 1rem;
-		gap: 1rem;
+    .login-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+        padding: 2rem 2.5rem;
+        padding-top: 1rem;
+        gap: 1rem;
 
-		border-radius: var(--border-radius);
-		box-shadow: var(--shadow-1);
-		background-color: var(--header-bg-color);
+        border-radius: var(--border-radius);
+        box-shadow: var(--shadow-1);
+        background-color: var(--header-bg-color);
 
-		.btn-github {
-			background: rgb(25, 25, 25);
-			color: white;
-		}
-		.btn-google {
-			background: #ff3434;
-			color: white;
-		}
-	}
+        .btn-github {
+            display: flex;
+            align-items: center;
+            background: rgb(25, 25, 25);
+            color: white;
+        }
+        .btn-google {
+            display: flex;
+            align-items: center;
+            background: #ff3434;
+            color: white;
+        }
+    }
 `;
 
 export default Login;
