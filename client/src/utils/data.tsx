081f4a06a450b2bc89f17e797dbbcd9086ee7c11
Neirea <neireasoultix@gmail.com>
2022-11-09 16:57:25 +0200

    updated dependencies

--- client/src/utils/data.tsx
+++ client/src/utils/data.tsx
@@ -3,27 +3,27 @@ import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
 import { FiMail } from "@react-icons/all-files/fi/FiMail";
 
 export const socialMediaLinks = [
-	{
-		image: <FaGithub size={"100%"} />,
-		name: "linkedin",
-		link: "https://www.linkedin.com/in/yevhenii-shumilin-2ab431188/",
-	},
-	{
-		image: <FaLinkedin size={"100%"} />,
-		name: "github",
-		link: "https://github.com/Neirea",
-	},
-	{
-		image: <FiMail size={"100%"} />,
-		name: "email",
-		link: "mailto:neirea@ukr.net",
-	},
+    {
+        image: <FaGithub size={"100%"} />,
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/yevhenii-shumilin-2ab431188/",
+    },
+    {
+        image: <FaLinkedin size={"100%"} />,
+        name: "github",
+        link: "https://github.com/Neirea",
+    },
+    {
+        image: <FiMail size={"100%"} />,
+        name: "email",
+        link: "mailto:neirea@ukr.net",
+    },
 ];
 export const menuItems = [
-	{ name: "HOME", link: "/" },
-	{ name: "BLOG", link: "/blog" },
-	{ name: "PROJECTS", link: "/project" },
-	{ name: "CONTACT", link: "/contact" },
-	{ name: "RESUME", link: "https://twitter.com" }, // add link to google docs later
+    { name: "HOME", link: "/" },
+    { name: "BLOG", link: "/blog" },
+    { name: "PROJECTS", link: "/project" },
+    { name: "CONTACT", link: "/contact" },
+    { name: "RESUME", link: "https://twitter.com" }, // add link to google docs later
 ];
-export const recaptchaKey = process.env.REACT_APP_RECAPTCHA_CLIENT; //personal recaptcha sit key
+export const recaptchaKey = process.env.REACT_APP_RECAPTCHA_CLIENT!; //personal recaptcha sit key
