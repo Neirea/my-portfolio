470f521ac3599d6bd1e609f82f8d1bd26739f1d4
Neirea <neireasoultix@gmail.com>
2022-11-18 23:18:09 +0200

    fixed contact links

--- client/src/utils/data.tsx
+++ client/src/utils/data.tsx
@@ -5,13 +5,13 @@ import { FiMail } from "@react-icons/all-files/fi/FiMail";
 export const socialMediaLinks = [
     {
         image: <FaGithub size={"100%"} />,
-        name: "linkedin",
-        link: "https://www.linkedin.com/in/yevhenii-shumilin-2ab431188/",
+        name: "github",
+        link: "https://github.com/Neirea",
     },
     {
         image: <FaLinkedin size={"100%"} />,
-        name: "github",
-        link: "https://github.com/Neirea",
+        name: "linkedin",
+        link: "https://www.linkedin.com/in/yevhenii-shumilin-2ab431188/",
     },
     {
         image: <FiMail size={"100%"} />,
@@ -24,6 +24,5 @@ export const menuItems = [
     { name: "BLOG", link: "/blog" },
     { name: "PROJECTS", link: "/project" },
     { name: "CONTACT", link: "/contact" },
-    { name: "RESUME", link: "https://twitter.com" }, // add link to google docs later
 ];
 export const recaptchaKey = process.env.REACT_APP_RECAPTCHA_CLIENT!; //personal recaptcha sit key
