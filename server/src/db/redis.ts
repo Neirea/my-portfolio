6044f36e3d365f0d49c45b3d6c4acb07976e065a
neirea <neirea@ukr.net>
2025-03-19 14:11:31 +0200

    refactor redis init

--- server/src/db/redis.ts
+++ server/src/db/redis.ts
@@ -1,13 +1,10 @@
 import { createClient } from "redis";
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const getRedisClient = () => {
-    const redisClient = createClient();
-    redisClient.on("error", (error) => {
-        console.error(error);
-        process.exit(1);
-    });
-    return redisClient;
-};
+const redisClient = createClient();
 
-export const redisClient = getRedisClient();
+redisClient.on("error", (error) => {
+    console.error(error);
+    process.exit(1);
+});
+
+export { redisClient };
