a3015d03acacc821c8a7b14d3da72cbeb3e052fa
neirea <neirea@ukr.net>
2025-03-30 17:38:39 +0300

    remove posthog, add umami

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import axios from "axios";
-import type { PostHogConfig } from "posthog-js";
-import { PostHogProvider } from "posthog-js/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 import { AppProvider } from "./store/AppContext";
-import { posthogHost, posthogKey, serverUrl } from "./utils/data";
-
-const options: Partial<PostHogConfig> = {
-    api_host: posthogHost,
-    persistence: "memory",
-    disable_session_recording: true,
-};
+import { serverUrl } from "./utils/data";
 
 axios.defaults.baseURL = serverUrl;
 axios.defaults.withCredentials = true;
@@ -28,15 +20,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <PostHogProvider apiKey={posthogKey} options={options}>
-            <BrowserRouter>
-                <QueryClientProvider client={queryClient}>
-                    <AppProvider>
-                        <App />
-                    </AppProvider>
-                    <ReactQueryDevtools initialIsOpen={false} />
-                </QueryClientProvider>
-            </BrowserRouter>
-        </PostHogProvider>
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <AppProvider>
+                    <App />
+                </AppProvider>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </BrowserRouter>
     </React.StrictMode>,
 );
